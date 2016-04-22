62b49b2b74c2acf67e49c8edebe951f0ec8f99e3
Erik Sundell <erik.i.sundell@gmail.com>
2016-04-22 08:29:25 +0200

    Forgot to add removed files

--- server/publications/game_list.js
+++
@@ -1,9 +0,0 @@
-import {Games} from '/lib/collections';
-import {Meteor} from 'meteor/meteor';
-import {check} from 'meteor/check';
-
-export default function () {
-  Meteor.publish('gameList.allGames', function () {
-    return Games.find({});
-  });
-}
